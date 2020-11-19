3557cc7871b731cb9f0e9f56b9b515e3be094f4d
Hernan <h.d.fiorito@gmail.com>
2020-11-19 01:17:21 -0300

    Instanciado objetos RedWifi,  ConsultorServidorTravis y Baliza en main

--- TP_Baliza/src/main.cpp
+++ TP_Baliza/src/main.cpp
@@ -2,17 +2,22 @@
 #include <WiFi.h>
 #include <HTTPClient.h>
 #include <Arduino_JSON.h>
-
+#include <RedWifi.cpp>
+#include <ConsultorServidorTravis.cpp>
+#include <Baliza.cpp>
 
 void setup() {
   Serial.begin(115200);
 
-  //RedWifi red("CAPPONI", "clau1963");
-  //red.conectar();
+  RedWifi red("CAPPONI", "clau1963");
+  red.conectar();
 
 }
 
 void loop() {
   
+  ConsultorServidorTravis consultor;
+  Baliza baliza;
+  JSONVar ultimoBuild = consultor.obtenerBuild();
 
 }
