e34f78259065ebff3dca128c8a88a0582ac8c0f9
Hernan <h.d.fiorito@gmail.com>
2020-11-19 01:57:00 -0300

    Añadida prueba para parpadear un led usando métodos de Baliza

--- TP_Baliza/src/main.cpp
+++ TP_Baliza/src/main.cpp
@@ -6,18 +6,24 @@
 #include <ConsultorServidorTravis.cpp>
 #include <Baliza.cpp>
 
+Baliza baliza;
+
 void setup() {
   Serial.begin(115200);
 
-  RedWifi red("CAPPONI", "clau1963");
-  red.conectar();
+  //RedWifi red("CAPPONI", "clau1963");
+  //red.conectar();
 
 }
 
 void loop() {
-  
-  ConsultorServidorTravis consultor;
-  Baliza baliza;
-  JSONVar ultimoBuild = consultor.obtenerBuild();
+
+  baliza.encenderLed(13);
+  delay(1000);
+  baliza.apagarLed(13);
+  delay(1000);
+
+  //ConsultorServidorTravis consultor;
+  //JSONVar ultimoBuild = consultor.obtenerBuild();
 
 }
